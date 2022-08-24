0504a81a171d74c1907b251d3007bad0fc8030da
estebanchiaro <estebanchiaromonte95@gmail.com>
2022-08-24 14:48:17 -0400

    update

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ window.addEventListener('load',()=>{
         
         const task_content_el= document.createElement('div')
         task_content_el.classList.add('content')
-        // task_content_el.innerText= task
 
         task_el.appendChild(task_content_el)
         
@@ -35,14 +34,16 @@ window.addEventListener('load',()=>{
         const task_actions_el = document.createElement('div')
         task_actions_el.classList.add('actions');
 
+
         const task_edit_el = document.createElement('button');
-        task_edit_el.classList.add('edit');
-         task_edit_el.innerText= 'Edit'
+            task_edit_el.classList.add('edit');
+             task_edit_el.innerText= 'Edit'
 
          const task_delete_el= document.createElement('button');
          task_delete_el.classList.add('delete');
-         task_delete_el.innerText='Delete'
+         task_delete_el.innerText='X'
 
+      
          task_actions_el.appendChild(task_edit_el)
          task_actions_el.appendChild(task_delete_el)
 
@@ -50,6 +51,7 @@ window.addEventListener('load',()=>{
 
         list_el.appendChild(task_el)
         input.value='';
+
 
         task_edit_el.addEventListener('click',(e)=>{
             if(task_edit_el.innerText.toLowerCase()=='edit')
@@ -86,6 +88,8 @@ window.addEventListener('load',()=>{
         task_delete_el.addEventListener('click',(e)=>{
             list_el.removeChild(task_el)
         })
+
+
 
     })
 })
